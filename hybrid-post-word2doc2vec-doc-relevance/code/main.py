749dedd15c05a7daa4c1555e27377f4be4754904
Soudeh-Jahanshahi <149694999+Soudeh-Jahanshahi@users.noreply.github.com>
2024-04-09 17:31:50 +0200

    Update main.py

--- hybrid-post-word2doc2vec-doc-relevance/code/main.py
+++ hybrid-post-word2doc2vec-doc-relevance/code/main.py
@@ -17,7 +17,7 @@ from nltk.corpus import stopwords
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-i", "--input", type=str, help="Path to input (train) .npy file")
+    parser.add_argument("-i", "--input", type=str, help="Path to input dataset .npy file")
     parser.add_argument("-gt", "--ground_truth", type=str, help="Path to ground truth .tsv file")
     parser.add_argument("-pj", "--params_json", type=str, help="File path to doc2vec hyperparameters JSON")
     parser.add_argument("-dict", "--MeShIDtoPMID", type=str, help="Path to input MeShIDtoPMID .tsv file.")
