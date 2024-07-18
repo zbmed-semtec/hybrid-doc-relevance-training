dab7fabda493f7d06d3755a9748edcd1a87429ee
rohitharavinder <rohitha0112@gmail.com>
2024-07-18 13:14:44 +0000

    🔨 update gdrive links for input data

--- Download_Dataset.sh
+++ Download_Dataset.sh
@@ -1,15 +1,21 @@
-sudo pip install gdown
+pip install gdown
 cd data
 
 mkdir -p Split_Dataset/Data
-mkdir -p Split_Dataset/Groundtruth
+mkdir -p Split_Dataset/Ground_truth
 
 cd Split_Dataset/Data/
-gdown https://drive.google.com/uc?id=1GkSgtiiX7BG5LygBq3I0HKRRGEBQ7_Lz -O test.npy
-gdown https://drive.google.com/uc?id=1oIPGaplji5jgn3qtlm_HE8lL5HTpeHXl -O train.npy
+# Post/Post-reduction annotation approaches
+# gdown https://drive.google.com/uc?id=19dk3ITChA7wKHJS_eSbzxad1DtIQ1dCx -O test.npy
+# gdown https://drive.google.com/uc?id=1jMSuZvHFTYdFdtmUclqy3X-jIXVNrmZo -O train.npy
+# gdown https://drive.google.com/uc?id=1tsz-xXeFCOa6b9JK8h6nt6yTrZru9oER -O valid.npy
+
+# Pre-annotation apporaches
+gdown https://drive.google.com/uc?id=1GkSgtiiX7BG5LygBq3I0HKRRGEBQ7_Lz -O test.npy 
+gdown https://drive.google.com/uc?id=1oIPGaplji5jgn3qtlm_HE8lL5HTpeHXl -O train.npy                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
 gdown https://drive.google.com/uc?id=1IhDxuD3jhhXwapeEOBmCgqcp4iFc_nWG -O valid.npy
 
-cd ../Groundtruth
+cd ../Ground_truth
 gdown https://drive.google.com/uc?id=15z0lWO2kUR8irIyzV5g3yn9wv2SaoRx0 -O test.tsv
 gdown https://drive.google.com/uc?id=1hTS76X6S_slnZZoZRBmlpYqq6T3vEiWF -O train.tsv
 gdown https://drive.google.com/uc?id=1FFXIH70kPKtwhtF7TvTQ7ue3M0zGSnuZ -O valid.tsv
